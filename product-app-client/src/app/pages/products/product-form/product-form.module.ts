15eea4bedb6a255602294794d96b7553a533a728
RichelmeBessa <richelmebessa@github.com>
2020-01-22 14:23:50 -0300

    Continue exercise of product-app with angular

---
+++ product-app-client/src/app/pages/products/product-form/product-form.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductFormPage } from './product-form.page';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductFormPage
+  }
+]
+
+@NgModule({
+  declarations: [ProductFormPage],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+
+export class ProductFormModule { }
--- product-app-client/src/app/pages/products/products.service.ts
+++ product-app-client/src/app/pages/products/products.service.ts
@@ -25,4 +25,8 @@ export class ProductsService {
       })
     );
   }
+
+  getProduct(id : string): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`);
+  }
 }
